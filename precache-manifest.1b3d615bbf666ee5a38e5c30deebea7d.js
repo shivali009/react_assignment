50e4c54ce562cd66ad4a6bfe0b9b191932a21b21
Shivali Maurya <addshivali009@gmail.com>
2021-04-14 23:06:36 +0530

    Updates

---
+++ precache-manifest.1b3d615bbf666ee5a38e5c30deebea7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5900d9581187b52f3a4de5b066bd17",
+    "url": "/react_assignment/index.html"
+  },
+  {
+    "revision": "b308a66e40a25105f7f7",
+    "url": "/react_assignment/static/css/main.06507b43.chunk.css"
+  },
+  {
+    "revision": "7f6ddbabfdcaf709d6f8",
+    "url": "/react_assignment/static/js/2.9695b7c6.chunk.js"
+  },
+  {
+    "revision": "b308a66e40a25105f7f7",
+    "url": "/react_assignment/static/js/main.66719d94.chunk.js"
+  },
+  {
+    "revision": "dd0e147dbfb00416aedb",
+    "url": "/react_assignment/static/js/runtime~main.5ba1f2d2.js"
+  }
+]);
